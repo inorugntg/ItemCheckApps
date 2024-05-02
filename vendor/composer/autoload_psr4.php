394f2a422afe3d47910597aca6932e93f9dd0430
inoru <inoru375@gmail.com>
2024-05-02 08:30:26 +0700

    qr code

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -73,6 +73,7 @@ return array(
     'Laravel\\Prompts\\' => array($vendorDir . '/laravel/prompts/src'),
     'Illuminate\\Support\\' => array($vendorDir . '/laravel/framework/src/Illuminate/Macroable', $vendorDir . '/laravel/framework/src/Illuminate/Collections', $vendorDir . '/laravel/framework/src/Illuminate/Conditionable'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
+    'Ibex\\CrudGenerator\\' => array($vendorDir . '/ibex/crud-generator/src'),
     'GuzzleHttp\\UriTemplate\\' => array($vendorDir . '/guzzlehttp/uri-template/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
